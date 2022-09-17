b4a06247339125e3841f53f62eb936bb3724c76b
JuanE <juanernestogranados@gmail.com>
2022-09-16 19:20:34 -0500

    added bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import 'react-bootstrap';
+import '../styles/globals.css';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap');
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
