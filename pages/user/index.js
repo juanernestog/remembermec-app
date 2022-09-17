7b460582ebd949e2115a247a976726c0cf94a4e3
JuanE <juanernestogranados@gmail.com>
2022-09-17 11:44:37 -0500

    Remove uneeded code due to layout

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,16 +1,12 @@
 import { useRouter } from 'next/router';
-import Footer from '../../components/Footer';
-import NavUser from '../../components/NavUser';
 
 export default function serPage() {
   const router = useRouter();
   const { userId } = router.query;
   return (
     <>
-      {/* <NavUser /> */}
       <h1>Users Page</h1>
       <p>Welcome {userId}</p>
-      <Footer />
     </>
   );
 }
