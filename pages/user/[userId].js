047a95c0ffd32bac4337e14f24d4172e9b4bb70d
JuanE <juanernestogranados@gmail.com>
2022-09-16 19:21:13 -0500

    aded user pages

--- pages/user/[userId].js
+++ pages/user/[userId].js
@@ -1,13 +1,38 @@
 import { useRouter } from 'next/router';
 
-export default function users() {
-  const router = useRouter();
-  const { userId } = router.query;
+export default function users({ dataUser, dataMachine }) {
+  // const router = useRouter();
+  // const { userId } = router.query;
+  console.log(dataMachine);
 
   return (
     <>
       <h1>Users</h1>
-      <p>Hello {userId}!</p>
+      <p>Hello {dataUser.data.name}!</p>
+      <h1> Tus maquinas</h1>
+      {dataMachine.data.map((machine) => {
+        return <h1 key={machine._id}> {machine.reference}</h1>;
+      })}
     </>
   );
+}
+
+export async function getServerSideProps({ params }) {
+  // primera carga de la pagina dentro de pagina
+  const user = await fetch(`http://localhost:3000/api/users/${params.userId}`, {
+    method: 'GET',
+  });
+  const dataUser = await user.json();
+
+  const machines = await fetch(`http://localhost:3000/api/machines`, {
+    method: 'GET',
+  });
+  const dataMachine = await machines.json();
+
+  return {
+    props: {
+      dataUser,
+      dataMachine,
+    },
+  };
 }
--- pages/user/index.js
+++ pages/user/index.js
@@ -1,5 +1,16 @@
 import { useRouter } from 'next/router';
+import Footer from '../../components/Footer';
+import NavUser from '../../components/NavUser';
 
-export default function userPage() {
-  return <h1>Users Page</h1>;
+export default function serPage() {
+  const router = useRouter();
+  const { userId } = router.query;
+  return (
+    <>
+      {/* <NavUser /> */}
+      <h1>Users Page</h1>
+      <p>Welcome {userId}</p>
+      <Footer />
+    </>
+  );
 }
