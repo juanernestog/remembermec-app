cfa40ba29cfbf8b13da2386fdb6cc05685827e26
JuanE <juanernestogranados@gmail.com>
2022-09-17 11:45:07 -0500

    Added formic and implemented a form

--- pages/machine/index.js
+++ pages/machine/index.js
@@ -1,30 +1,104 @@
-import axios from 'axios';
+const axios = require('axios').default;
+import { Formik } from 'formik';
+import { Card, Row, Col } from 'react-bootstrap';
 
 export default function Machine() {
   const handleSubmit = (event) => {
     event.preventDefault();
     //aquí va el post
-    axios('/', 'post', { post: event }, functions.handleSubmit);
+    axios
+      .post('http://localhost:3000/api/machines', {
+        reference: event.reference,
+        lastName: 'Flintstone',
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      action="http://localhost:3000/api/machines"
-      method="post"
-    >
-      <title>Create new Machine</title>
-      <label htmlFor="createMachine">reference</label>
-      <input type="text" name="reference" required />
-      <label htmlFor="">Marca</label>
-      <input type="text" name="brand" required />
-      <label htmlFor="name"> Año del modelo </label>
-      <input type="number" name="model" max="2100" min="1900" required />
-      <label htmlFor="name">Descrpcion</label>
-      <input type="text-area" name="description" required />
-      <label htmlFor="name">Placa</label>
-      <input type="text" name="numberPlate" required />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <div>
+        <h1>Añade una nueva maquina</h1>
+        <Formik
+          initialValues={{
+            reference: '',
+            brand: '',
+            model: '',
+            description: '',
+            numberPlate: '',
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+            console.log(JSON.stringify(values, null, 2));
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit} className="flex">
+              <title>Create new Machine</title>
+              <label htmlFor="createMachine">reference</label>
+              <input
+                type="text"
+                name="reference"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              <label htmlFor="">Marca</label>
+              <input
+                type="text"
+                name="brand"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              <label htmlFor="name"> Año del modelo </label>
+              <input
+                type="text"
+                name="model"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              <label htmlFor="name">Descrpcion</label>
+              <input
+                type="text-area"
+                name="description"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+              />
+              <label htmlFor="name">Placa</label>
+              <input
+                type="text"
+                name="numberPlate"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+              />
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </form>
+          )}
+        </Formik>
+      </div>
+      <style></style>
+    </>
   );
 }
